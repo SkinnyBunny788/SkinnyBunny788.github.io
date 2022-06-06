05162b7a308c782ee3c774ee7dd8b76a4260c4c1
SkinnyBunny788 <47288457+SkinnyBunny788@users.noreply.github.com>
2022-06-06 20:50:27 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+
+function startTimer() {
+
+    setInterval(function timer() { 
+        document.getElementById('timerTitle').style = 'visibility: visible'
+        document.getElementById('timerTitle').innerText = document.getElementById('input').value;
+        let currentDate = new Date()
+        let goalDate = new Date(document.getElementById('input').value)
+        let diff = goalDate - currentDate;
+
+
+        let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((diff / (1000 * 60 * 60) % 24));
+        let minutes = Math.floor((diff / (1000 * 60) % 60));
+        let seconds = Math.round((diff / 1000) % 60);
+
+        document.getElementById('days').innerHTML = days
+        if (hours < 10) {
+            document.getElementById('hours').innerHTML = '0' + hours
+        } else {
+            document.getElementById('hours').innerHTML = hours
+        }
+        if (minutes < 10) {
+            document.getElementById('minutes').innerHTML = '0' + minutes
+        } else {
+            document.getElementById('minutes').innerHTML = minutes
+        }
+        if (seconds < 10) {
+            document.getElementById('seconds').innerHTML = '0' + seconds
+        }  else {
+            document.getElementById('seconds').innerHTML = seconds
+        }
+    }, 1000);
+}
+ 
