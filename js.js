da2c5cc0b36cbc09eeab1738924aacf0342395a3
SkinnyBunny788 <francolkk78@gmail.com>
2022-07-22 02:45:41 -0300

    Overall readability

--- js.js
+++ js.js
@@ -1,17 +1,16 @@
 function startTimer() {
     setInterval(function timer() { 
+        const timerTitle = document.getElementById('input').value,
+              goalDate = new Date(document.getElementById('input').value),
+              currentDate = new Date(),
+              diff = goalDate - currentDate,
+              days = Math.floor(diff / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((diff / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((diff / (1000 * 60) % 60)),
+              seconds = Math.round((diff / 1000) % 60);
+              
         document.getElementById('timerTitle').style = 'visibility: visible'
-        let timerTitle = document.getElementById('input').value
         document.getElementById('timerTitle').innerText = timerTitle.slice(0,timerTitle.indexOf('T'));
-        let currentDate = new Date()
-        let goalDate = new Date(document.getElementById('input').value)
-        let diff = goalDate - currentDate;
-
-        let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((diff / (1000 * 60 * 60) % 24));
-        let minutes = Math.floor((diff / (1000 * 60) % 60));
-        let seconds = Math.round((diff / 1000) % 60);
-
         document.getElementById('days').innerHTML = days
         if (hours < 10) document.getElementById('hours').innerHTML = `0${hours}`
         else document.getElementById('hours').innerHTML = hours
